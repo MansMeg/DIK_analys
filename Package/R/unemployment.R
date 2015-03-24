bfbdd70a1a692331492ce1d947ab516fe5ff3ba6
Måns Magnusson <mons.magnusson@gmail.com>
2015-03-24 22:39:26 +0100

    Updated package to v 0.1.1

--- Package/R/unemployment.R
+++ Package/R/unemployment.R
@@ -167,39 +167,3 @@ calc_ers_stat <- function(AEA_data){
   return(res)  
 }
 
-
-#' @title
-#' Classifies based on DIK interest groups
-#' 
-#' @param x variable to classify
-#' @param type which classification to use ("utbildningsgrupp" or "intressegrupp")
-#' 
-#' @details
-#' The functions classifies using the classifiers found here:
-#' \url{https://github.com/MansMeg/DIK_analys/tree/master/Classification}
-#' 
-#' @export
-dik_classify <- function(x, type = c("utbildningsgrupp", "intressegrupp")){
-  x <- as.factor(x)
-  if(substr(type[1], 1, 3) == "utb"){
-    class_url <- "https://raw.githubusercontent.com/MansMeg/DIK_analys/master/Classification/Utbildningsgrupper.csv"
-  } else if (substr(type[1], 1, 3) == "int"){
-    class_url <- "https://raw.githubusercontent.com/MansMeg/DIK_analys/master/Classification/Intressegrupper.csv"
-  }
-  class_table <- suppressMessages(repmis::source_data(url = class_url, stringsAsFactors = FALSE))
-  class_table[,2] <- tolower(class_table[,2])
-  names(class_table)[2] <- "label"
-  
-  add_label_df <- data.frame(rowno=1:length(levels(x)), Label = stringr::str_trim(levels(x)), label = tolower(stringr::str_trim(levels(x))), stringsAsFactors = FALSE)
-  add_label_df <- merge(add_label_df, class_table, all.x = TRUE)
-  not_in_class <- is.na(add_label_df[,4])
-  
-  if(any(not_in_class)) {
-    warning("The following classes has not been classified:\n'",
-            paste(add_label_df$Label[not_in_class], collapse="'\n'"), "'")
-    add_label_df[not_in_class, 4] <- "NOT CLASSIFIED"
-  }
-  levels(x) <- add_label_df[order(add_label_df$rowno), 4]
-  factor(as.character(x))
-}
-

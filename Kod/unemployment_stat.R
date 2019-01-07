c5ca88764aaed7f90dadf5a6274e5b7c7cde8547
Måns Magnusson <mons.magnusson@gmail.com>
2019-01-07 17:57:50 +0200

    Small fixes in file

--- Kod/unemployment_stat.R
+++ Kod/unemployment_stat.R
@@ -9,12 +9,14 @@ devtools::install_github(repo = "MansMeg/DIK_analys", subdir = "Package")
 library(DIK)
 
 # Ange sökväg till AEA-filen
+# file_path <- "test_file/arbetsloshetsstatistik_36_201804_20181016.csv"
 file_path <- file.choose()
-file_path <- "test_file/arbetsloshetsstatistik_36_201804_20181016.csv"
+
 
 # Läs in datan i R
+# AEA_data <- read_AEA_file(file_path, stat_var = c("stat1"="intrgrp", "stat2"="sektor", "stat3"="anst", "stat4" = "utbniva", "stat5" = "utbgrp"))
 AEA_data <- read_AEA_file(file_path)
-AEA_data <- read_AEA_file(file_path, stat_var = c("stat1"="intrgrp", "stat2"="sektor", "stat3"="anst", "stat4" = "utbniva", "stat5" = "utbgrp"))
+
 
 # Gör beräkningar/producera statistik
 dik_stat <- aggr_unempl_data(AEA_data = AEA_data)
@@ -22,4 +24,6 @@ dik_stat <- aggr_unempl_data(AEA_data = AEA_data)
 # Lägg till i statistikdatabasen
 write_dik_stat_csv(dik_stat, db_path = "Data/")
 
+# Zip and then send
+zip(make.names(paste0("stat_files_", Sys.time(), ".zip")), dir("Data/", full.names = TRUE)[stringr::str_detect(dir("Data/"),"\\.csv")])
 

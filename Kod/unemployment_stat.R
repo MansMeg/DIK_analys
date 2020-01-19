8a8a1895182f54e7b6a86f0119a127a9353dc643
Måns Magnusson <mons.magnusson@gmail.com>
2020-01-19 14:58:43 +0200

    Clarifications

--- Kod/unemployment_stat.R
+++ Kod/unemployment_stat.R
@@ -12,14 +12,24 @@ library(DIK)
 # file_path <- "test_file/arbetsloshetsstatistik_36_201804_20181016.csv"
 file_path <- file.choose()
 
-
 # Läs in datan i R
 # AEA_data <- read_AEA_file(file_path, stat_var = c("stat1"="intrgrp", "stat2"="sektor", "stat3"="anst", "stat4" = "utbniva", "stat5" = "utbgrp"))
 AEA_data <- read_AEA_file(file_path)
-# Can warn for missing values.
+
+# Can warn for missing values. The one below are expected but should be reported.
+# Warning messages:
+#  1: The following variables cannot be identified: stat1, stat2, stat4 
+#  2: Report: variable 'utbniva' is now missing and will not be used. 
+
 
 # Gör beräkningar/producera statistik
-dik_stat <- aggr_unempl_data(AEA_data = AEA_data)
+dik_stat <- aggr_unempl_data(AEA_data = AEA_data, 
+                             classification_source_folder = "Classification/")
+
+# Expected warnings:
+# Warning messages:
+#   1: 'ers_by_interest_group.csv' cannot be computed ('ers', 'utbprogram', 'intrgrp' or 'infopost' is missing) 
+#   2: 'ers_by_education_level.csv' cannot be computed ('ers', 'utbprogram', 'utbniva', or 'infopost' is missing) 
 
 # Lägg till i statistikdatabasen
 write_dik_stat_csv(dik_stat, db_path = "Data/")
